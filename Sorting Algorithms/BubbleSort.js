2614e952b0c6eade9a31f181f0d73578ea4b47f9
Tushar Rajaram Rao <49467516+TusharRao8334@users.noreply.github.com>
2022-09-23 10:03:33 +0530

    Bubble sort

--- Sorting Algorithms/BubbleSort.js
+++ Sorting Algorithms/BubbleSort.js
@@ -1,12 +1,17 @@
 //  Bubble Sort in js
 // function def
-
-function bubbleSort() {
-
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j + 1] < arr[j]) {
+                [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]]; //swapping of array elements
+            }
+        }
+    }
+    return arr;
 }
 
-
-
-
 // driver code
-let arr = [5, 98, 65, 12, 45, 63, 9, 15];
+let arr = [0, 5, 98, 65, 112, 12, 45, 63, 9, 15];
+let ans = bubbleSort(arr);
+console.log(ans);
