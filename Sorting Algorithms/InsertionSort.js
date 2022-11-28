049c8ff2d0cda79b2f47a42d6fb3d9426c95f28c
Tushar Rajaram Rao <49467516+TusharRao8334@users.noreply.github.com>
2022-11-28 09:09:56 +0530

    Insertion sort

---
+++ Sorting Algorithms/InsertionSort.js
@@ -0,0 +1,11 @@
+function insertionSearch(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let key = arr[i];
+        let j;
+
+    }
+
+}
+
+console.log(insertionSearch(a));
+let a = [2, 1, 98, 65, 45, 23, 11];
